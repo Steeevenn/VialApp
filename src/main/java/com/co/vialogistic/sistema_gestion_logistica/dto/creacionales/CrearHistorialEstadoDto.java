f3821f1bb960aadbf9b0d78639d3dbf893e6cad6
Michael Motivar <michaelbeltran80@gmail.com>
2025-09-01 22:33:45 -0500

    Implementacion de historial de recolecciones con dtos y capa de servicio agregada correctamente

---
+++ src/main/java/com/co/vialogistic/sistema_gestion_logistica/dto/creacionales/CrearHistorialEstadoDto.java
@@ -0,0 +1,23 @@
+package com.co.vialogistic.sistema_gestion_logistica.dto.creacionales;
+
+import com.co.vialogistic.sistema_gestion_logistica.model.enums.EstadoRecoleccion;
+import org.apache.logging.log4j.message.Message;
+
+import javax.validation.constraints.NotNull;
+
+public record CrearHistorialEstadoDto(
+
+        @NotNull (message = "ID de recoleccion obligatorio")
+        Long recoleccionId,
+
+        EstadoRecoleccion estadoAnterior,
+
+        @NotNull(message = "Estado nuevo obligatorio")
+        EstadoRecoleccion estadoNuevo,
+
+        Long cambiadoPorUsuarioId,
+
+        String motivoCambio
+
+) {
+}
--- src/main/java/com/co/vialogistic/sistema_gestion_logistica/model/entity/HistorialEstadoRecoleccion.java
+++ src/main/java/com/co/vialogistic/sistema_gestion_logistica/model/entity/HistorialEstadoRecoleccion.java
@@ -9,8 +9,6 @@ import java.time.OffsetDateTime;
 
 @Entity
 @Table(name = "historial_estados_recoleccion")
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class HistorialEstadoRecoleccion {
@@ -41,6 +39,65 @@ public class HistorialEstadoRecoleccion {
     @CreationTimestamp
     @Column(name = "timestamp_cambio", nullable = false, updatable = false, columnDefinition = "TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP")
     private OffsetDateTime timestampCambio;
+
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recoleccion getRecoleccion() {
+        return recoleccion;
+    }
+
+    public void setRecoleccion(Recoleccion recoleccion) {
+        this.recoleccion = recoleccion;
+    }
+
+    public EstadoRecoleccion getEstadoAnterior() {
+        return estadoAnterior;
+    }
+
+    public void setEstadoAnterior(EstadoRecoleccion estadoAnterior) {
+        this.estadoAnterior = estadoAnterior;
+    }
+
+    public EstadoRecoleccion getEstadoNuevo() {
+        return estadoNuevo;
+    }
+
+    public void setEstadoNuevo(EstadoRecoleccion estadoNuevo) {
+        this.estadoNuevo = estadoNuevo;
+    }
+
+    public Usuario getCambiadoPorUsuario() {
+        return cambiadoPorUsuario;
+    }
+
+    public void setCambiadoPorUsuario(Usuario cambiadoPorUsuario) {
+        this.cambiadoPorUsuario = cambiadoPorUsuario;
+    }
+
+    public String getMotivoCambio() {
+        return motivoCambio;
+    }
+
+    public void setMotivoCambio(String motivoCambio) {
+        this.motivoCambio = motivoCambio;
+    }
+
+    public OffsetDateTime getTimestampCambio() {
+        return timestampCambio;
+    }
+
+    public void setTimestampCambio(OffsetDateTime timestampCambio) {
+        this.timestampCambio = timestampCambio;
+    }
 }
 
 
